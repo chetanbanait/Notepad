2747f7b34e238b09b4240d639dc02872a414c5ba
CHETAN BANAIT <121280853+chetanbanait@users.noreply.github.com>
2025-09-19 11:06:51 +0530

    Update Notepad.java

--- Notepad/src/main/java/com/l33t_c0d3r_66/notepad/Notepad.java
+++ Notepad/src/main/java/com/l33t_c0d3r_66/notepad/Notepad.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package com.l33t_c0d3r_66.notepad;
 
